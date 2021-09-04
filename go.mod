86966bc4059b95dae06b08f2563993f8ef39e247
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2021-09-05 00:20:24 +0300

    install testify

--- go.mod
+++ go.mod
@@ -7,5 +7,6 @@ require (
 	github.com/jmoiron/sqlx v1.3.4 // indirect
 	github.com/joho/godotenv v1.3.0 // indirect
 	github.com/lib/pq v1.10.2 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
 	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
 )
--- pkg/repository/bot_postgres_test.go
+++ pkg/repository/bot_postgres_test.go
@@ -1,13 +1,12 @@
 package repository
 
 import (
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestGenerateConditionForDeactivatingSubscribers(t *testing.T) {
 	result := GenerateConditionForDeactivatingSubscribers([]int64{1, 2, 3})
 	expected := "where tg_chat_id=1 or tg_chat_id=2 or tg_chat_id=3"
-	if result != expected {
-		t.Errorf("Expected '%s', got '%s'", expected, result)
-	}
+	assert.Equalf(t, expected, result, "")
 }
